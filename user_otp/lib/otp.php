0beb984c9b50d89c95f736c09f908212f23b1d55
loki36 <fbongrand@free.fr>
2013-11-16 22:56:05 +0100

    bug fix user_ldap backend #26

--- user_otp/lib/otp.php
+++ user_otp/lib/otp.php
@@ -72,6 +72,30 @@ class OC_USER_OTP extends OC_User_Backend{
         foreach ($usedBackends as $backend){
           OC_Log::write('user_otp', 'instance '.$backend.' backend.', OC_Log::DEBUG);
           self::$_backends[$backend] = new $backend();
+          
+          if(self::$_backends[$backend] instanceof  OCA\user_ldap\USER_LDAP){
+			  //OC_Log::write('OC_USER_OTP', __FUNCTION__.'().', OC_Log::DEBUG);
+			  $configPrefixes = OCA\user_ldap\lib\Helper::getServerConfigurationPrefixes(true);
+				if(count($configPrefixes) == 1) {
+					$connector = new OCA\user_ldap\lib\Connection($configPrefixes[0]);
+					$userBackend  = new OCA\user_ldap\USER_LDAP();
+					$userBackend->setConnector($connector);
+					$groupBackend = new OCA\user_ldap\GROUP_LDAP();
+					$groupBackend->setConnector($connector);
+				} else {
+					$userBackend  = new OCA\user_ldap\User_Proxy($configPrefixes);
+					$groupBackend  = new OCA\user_ldap\Group_Proxy($configPrefixes);
+				}
+				
+				self::$_backends[$backend] = $userBackend;
+
+				if(count($configPrefixes) > 0) {
+					// register user backend
+					//~ OC_User::useBackend($userBackend);
+					OC_Group::useBackend($groupBackend);
+				}
+		  }
+          
         }
       }
     }
@@ -176,21 +200,6 @@ class OC_USER_OTP extends OC_User_Backend{
 		
 		$reflectionMethod = new ReflectionMethod(get_class($userBackend),$name);
 		return $reflectionMethod->invokeArgs($userBackend,$arguments);
-	}
-	
-	public function __get($name){
-		//OC_Log::write('OC_USER_OTP', $name.'().', OC_Log::DEBUG);
-		$userBackend=$this->getRealBackend(OCP\User::getUser());
-    //var_dump($userBackend);
-		if($userBackend===null){
-			return false;
-		}
-		
-		if(isset($userBackend->$name)){
-			return $userBackend->$name;
-		}else{
-			return false;
-		}
 	}
 
     /**
