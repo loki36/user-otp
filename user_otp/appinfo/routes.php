f31df6a5ba911f65029d314b3386c5d732ab8646
loki36 <fbongrand@free.fr>
2014-02-26 20:23:08 +0100

    enhancement #40 first commit

--- user_otp/appinfo/routes.php
+++ user_otp/appinfo/routes.php
@@ -1,3 +1,4 @@
 <?php
 
 $this->create('user_otp_qrcode','qrcode.php')->actionInclude('user_otp/qrcode.php');
+$this->create('user_otp_list_users','list_users')->actionInclude('user_otp/list_users.php');
